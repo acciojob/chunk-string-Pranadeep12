b0c98cc41ad2f17b19237194b3981e221cd949f5
Pranadeep12 <129034610+Pranadeep12@users.noreply.github.com>
2025-08-26 18:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function stringChop(str, size) {
   // your code here
+	let chunks = [];
+	 for (let i = 0; i < str.length; i += size) {
+    // Slice the string to get a chunk and push it to the array
+    chunks.push(str.slice(i, i + size));
+  }
+return chunks.join('\n');
 }
 
 // Do not change the code below
